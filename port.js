8142db5d6c9999a8918baf3e90f131cda5dd2b97
frenchfritzy <fritz32973@gmail.com>
2025-10-04 09:58:26 +0800

    Add files via upload

---
+++ port.js
@@ -0,0 +1,26 @@
+const username = document.getElementById("user");
+const password = document.getElementById("pass");
+const signBTN = document.getElementById("signBTN");
+const loginAlert = document.getElementById("loginAlert");
+
+signBTN.onclick = function() {
+    let user = "Fritz";
+    let pass = "123";
+
+    if (username.value === user && password.value === pass) {
+        window.location.href = "Portfolio.html";
+        loginAlert.text = "Login Successfully!";
+        loginAlert.classList.add("show");
+
+        setTimeout(() => {
+            loginAlert.classList.remove("show");
+        }, 3000);
+    } else {
+        loginAlert.textContent = "Invalid Credential. Ask Fritz to Proceed!";
+        loginAlert.classList.add("show");
+
+        setTimeout(() => {
+            loginAlert.classList.remove("show");
+        }, 3000);
+    }
+};
